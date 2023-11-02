c03eddf4882e80305652f14fd3129061041069a2
ibrahimkafkasli <ibrahim.kafkasli@hotmail.com>
2023-11-02 23:03:15 +0300

    initial commit

---
+++ src/test/java/com/innogrove/java21/Java21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innogrove.java21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
